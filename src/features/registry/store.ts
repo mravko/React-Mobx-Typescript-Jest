ca991294700c68975f4d9470d0e69ea88ef39783
mravko <vuckovikmarko@outlook.com>
2019-08-22 01:40:48 +0200

    Webpack init config

---
+++ src/features/registry/store.ts
@@ -0,0 +1,5 @@
+import { observable } from 'mobx'
+
+class RegistryStore {
+	@observable name: string;
+}
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+   <div>Hi marko 50</div>,
+    document.getElementById("root")
+);
