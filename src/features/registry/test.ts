f44c7275b3865b3dcb417845c5bf68de825f986a
mravko <vuckovikmarko@outlook.com>
2019-08-22 14:19:44 +0200

    tests

--- src/features/registry/test.ts
+++ src/features/registry/test.ts
@@ -2,9 +2,12 @@ import { RegistryStore } from "./store"
 
 describe("TodoStore", () => {
 	it("creates new todos", () => {
+
 	  const store = new RegistryStore();
+
 	  expect(store.name).toBe(undefined);
 	  store.changeName("marko");
 	  expect(store.name).toBe("marko");
+	  
 	})
   })
