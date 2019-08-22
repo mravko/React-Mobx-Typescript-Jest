628825e89ad1a5ea1508950eefd60076cdd62b40
mravko <vuckovikmarko@outlook.com>
2019-08-22 14:15:04 +0200

    jest tests

---
+++ src/features/registry/test.ts
@@ -0,0 +1,10 @@
+import { RegistryStore } from "./store"
+
+describe("TodoStore", () => {
+	it("creates new todos", () => {
+	  const store = new RegistryStore();
+	  expect(store.name).toBe(undefined);
+	  store.changeName("marko");
+	  expect(store.name).toBe("marko");
+	})
+  })
