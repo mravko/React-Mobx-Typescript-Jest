de9f42239f747e803cc59595930490f275f4d795
mravko <vuckovikmarko@outlook.com>
2019-08-22 10:28:56 +0200

    Fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,18 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+class App extends React.Component {
+    num: number;
+    render() {
+        this.num = 5;
+        return <div>hello from app {this.num} </div>;
+    }
+}
+
 ReactDOM.render(
-   <div>Hi marko 50</div>,
+    <div>
+        <App />
+    </div>,
     document.getElementById("root")
 );
+
